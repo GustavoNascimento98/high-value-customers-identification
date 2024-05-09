fe1eb56c73832fa870056008b591322b972be6b9
GustavoNascimento98 <estudosgustavo@gmail.com>
2024-05-09 10:33:31 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project has a goal to discovery similarities between people that joined a loyalty program',
+    author='Gustavo Nascimento',
+    license='MIT',
+)
